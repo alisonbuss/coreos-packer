7112cf8af342cd8d9c6b0130decd1eb801ea14f5
alisonbuss <alisonbuss.dev@gmail.com>
2017-12-17 04:29:45 -0300

    novos arquivos!!!  :-)

--- build.sh
+++ build.sh
@@ -9,9 +9,68 @@
 #-------------------------------------------------------------#
 
 function startBuilding {
- 
+
+     # @descr: 
+    __convertConfigIntoIgnition() {
+        local configSource="./container-linux-config/source/ignition-source.yml";
+        local convertedPath="./container-linux-config/ignition";
+
+        echo "Convert a Container Linux Config into Ignition!!";
+
+        rm -rf ${convertedPath}/*;
+
+        sleep 1s;
+
+        echo "Converting to (packet)...";
+        ct --platform=packet < "${configSource}" > "${convertedPath}/ignition-packet.json" --pretty;
+        ct --platform=packet < "${configSource}" > "${convertedPath}/ignition-packet.min.json";
+
+        echo "Converting to (vagrant-virtualbox)...";
+        ct --platform=vagrant-virtualbox < "${configSource}" > "${convertedPath}/ignition-vagrant.json" --pretty;
+        ct --platform=vagrant-virtualbox < "${configSource}" > "${convertedPath}/ignition-vagrant.min.json";
+
+        echo "Converting to (digitalocean)...";
+        ct --platform=digitalocean < "${configSource}" > "${convertedPath}/ignition-digitalocean.json" --pretty;
+        ct --platform=digitalocean < "${configSource}" > "${convertedPath}/ignition-digitalocean.min.json";
+
+        echo "Converting to (ec2)...";
+        ct --platform=ec2 < "${configSource}" > "${convertedPath}/ignition-ec2.json" --pretty;
+        ct --platform=ec2 < "${configSource}" > "${convertedPath}/ignition-ec2.min.json";
+
+        echo "Converting to (gce)...";
+        ct --platform=gce < "${configSource}" > "${convertedPath}/ignition-gce.json" --pretty;
+        ct --platform=gce < "${configSource}" > "${convertedPath}/ignition-gce.min.json";
+
+        echo "Converting to (azure)...";
+        ct --platform=azure < "${configSource}" > "${convertedPath}/ignition-azure.json" --pretty;
+        ct --platform=azure < "${configSource}" > "${convertedPath}/ignition-azure.min.json";
+    }
+
+    # @descr: 
+    __initialize() {
+        __convertConfigIntoIgnition;
+
+        # set parameters
+        local _params="$@";
+        case $_params in
+            *--run*|*-r*) { 
+                clear;
+            };;
+            *--list*|*-l*) { 
+                clear;
+            };;
+            *--help*|*-help*|*-h*|*help*) {
+                clear;
+            };;
+            *--version*|*-version*|*-v*|*version*) {
+                clear;
+            };;
+        esac
+    }
+
+    __initialize "$@"; 
 } 
 
-startBuilding;
+startBuilding "$@" | tee -a "${0##*/}.log";
 
 exit 0;
