6b769f9b2e99abe9814b8b221cc9ef46e88418cb
alisonbuss <alisonbuss.dev@gmail.com>
2018-03-06 04:07:04 -0300

    Add app Rkt

---
+++ pre-provision/shell-script/install-rkt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#-----------------------|DOCUMENTATION|-----------------------#
+# @descr:
+# @fonts: 
+#-------------------------------------------------------------#
+
+# @descr: Main function of the script, it runs automatically on the script call.
+# @param: 
+#    $@ | array: (*)
+function StartInstallation {
+
+    printf '%b\n' "CoreOS already has (Rkt) installed on your system...";
+    
+    rkt version;
+    
+} 
+
+# @descr: Call of execution of the script's main function.
+StartInstallation "$@";
+
+# @descr: Finishing the script!!! :P
+exit 0;
